c2820bc1a8751da2eedf150701f5fef2449a51ed
Fajar <fajaruddin.soleh@gmail.com>
2024-11-29 20:39:52 +0700

    ganti route

--- src/layout/home.jsx
+++ src/layout/home.jsx
@@ -10,7 +10,7 @@ const home = () => {
         <div className="flex-1">
           <Link to="/pokemon" className="px-1 hover:bg-gray-700">API Pokemon</Link>
           <Link to="/starwars" className="px-1 hover:bg-gray-700">API starwars</Link>
-          <Link to='/linktree' className="px-1 hover:bg-gray-700">Linktree</Link>
+          <Link to='/' className="px-1 hover:bg-gray-700">Linktree</Link>
         </div>
       </div>
       <Outlet />
